386a4e9d5c9a55f39efc4cda1f033b047d543054
Jamie McCrae <spam@helper3000.net>
2024-06-10 18:56:45 +0100

    Re-order sensor functions

--- app/src/sensor.h
+++ app/src/sensor.h
@@ -4,8 +4,8 @@
  * All right reserved. This code is NOT apache or FOSS/copyleft licensed.
  */
 
-/* Fetch sensor readings in XX.YY format for temperature and XX format for humidity */
-int sensor_fetch_readings(int8_t *temperature, int8_t *humidity);
-
 /* Setup sensor module */
 int sensor_setup(void);
+
+/* Fetch sensor readings in XX.YY format for temperature and XX format for humidity */
+int sensor_fetch_readings(int8_t *temperature, int8_t *humidity);
